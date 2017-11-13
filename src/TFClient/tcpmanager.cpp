ddb031765bd2c69e79fb9ba355e032f0d2687a84
joonardo <joonardob@gmail.com>
2017-11-13 19:16:50 +0200

    Added skeleton for TCPManager on client-side.

---
+++ src/TFClient/tcpmanager.cpp
@@ -0,0 +1,6 @@
+#include "tcpmanager.h"
+
+TCPManager::TCPManager()
+{
+
+}
---
+++ src/TFClient/tcpmanager.h
@@ -0,0 +1,11 @@
+#ifndef TCPMANAGER_H
+#define TCPMANAGER_H
+
+
+class TCPManager
+{
+public:
+    TCPManager();
+};
+
+#endif // TCPMANAGER_H
