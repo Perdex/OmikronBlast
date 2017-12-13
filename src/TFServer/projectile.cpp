ab579c51725409b5028ef90f60eef7df590796a3
Joonatan Bergholm <joonatan.bergholm@aalto.fi>
2017-12-13 15:24:27 +0200

    Fixed collision detection between player and projectile.

--- src/TFServer/projectile.cpp
+++ src/TFServer/projectile.cpp
@@ -25,9 +25,10 @@ projectile::~projectile(){}
 
 bool projectile::doStep(int dt){
     if(!isActive) return false;
+    const int playerHitboxW = 30, playerHitboxH = 70;
     for(player *p: mainWindow->getPlayers().values()) {
         QRectF self(x - RADIUS, y - RADIUS, 2*RADIUS, 2*RADIUS);
-        QRectF pl(p->getHorizontalPos(), p->getVerticalPos(), 40, 80);
+        QRectF pl(p->getHorizontalPos() - playerHitboxW/2, p->getVerticalPos() - playerHitboxH/2, playerHitboxW, playerHitboxH);
 
         if(self.intersects(pl)) {
             hitPlayer(*p);
