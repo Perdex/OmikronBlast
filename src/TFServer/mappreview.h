d7acbf8af671b0ed28a29561a0ecce2ff5cd5b95
Perttu Yli-Opas <perttu.yli-opas@aalto.fi>
2017-12-14 15:58:54 +0200

    Added markers to server minimap

--- src/TFServer/mappreview.h
+++ src/TFServer/mappreview.h
@@ -2,17 +2,23 @@
 #define MAPPREVIEW_H
 
 #include <QGraphicsView>
+#include <QGraphicsItem>
 
 class QGraphicsScene;
+class player;
 
 class MapPreview : public QGraphicsView
 {
 public:
     MapPreview(QWidget *parent = nullptr);
+    ~MapPreview();
     void setMap(bool*);
+    void addPlayer(player *p);
+    void updatePlayers(QVector<player*> &v);
 private:
     QGraphicsScene scene;
     const int W = 40, H = 40;
+    QMap<qint16, QGraphicsItem*> playerItems;
 };
 
 #endif // MAPPREVIEW_H
