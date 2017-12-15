50917eecac1b3edb74de0e05e01ce47012eaa64b
Perttu Yli-Opas <perttu.yli-opas@aalto.fi>
2017-12-15 17:43:04 +0200

    Modified player removal

--- src/TFServer/projectile.cpp
+++ src/TFServer/projectile.cpp
@@ -71,7 +71,6 @@ void projectile::move(int dt, TCPManager &mgr){
 void projectile::hitPlayer(player& victim)
 {
     victim.die();
-    mainWindow->remove(&victim);
     if(victim.getId() != owner->getId())
     {
         owner->getPoint();
